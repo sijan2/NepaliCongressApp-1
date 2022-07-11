56c2fe227d23ea3916da4da637724ed9e3679918
Nitesh Raj Khanal <nitesh.khanal19@gmail.com>
2022-07-11 13:19:16 +0545

    feat: Project initialized with path aliases setup #1

---
+++ babel.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    require.resolve('babel-plugin-module-resolver'),
+    {
+      cwd: 'babelrc',
+      extensions: ['.js', '.jsx', '.ts', '.tsx', '.json', '.svg˝'],
+      root: ['./src'],
+      alias: {
+        '@assets': './src/assets',
+        '@components': './src/components',
+        '@constants': './src/constants',
+        '@navigation': './src/navigation',
+        '@screens': './src/screens',
+        '@utils': './src/utils',
+      },
+    },
+  ],
+};
